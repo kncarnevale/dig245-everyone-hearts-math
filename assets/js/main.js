b3228ba2b3ef42efb8026f3a560b40beec71731e
Kate Carnevale <kacarnevale@davidson.edu>
2022-09-19 09:21:46 -0400

    class updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
 
 /* javascript */
+
+//$("h1").hide();
+
+let username ="";
+
+$(".submit").click(function(e){
+  e.preventDefault();
+  username=$(".username").val();
+  console.log("username is " + username);
+});
